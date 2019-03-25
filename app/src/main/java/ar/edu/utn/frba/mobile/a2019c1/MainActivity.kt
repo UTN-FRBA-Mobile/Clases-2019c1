773eb50e1859ce2c75fcd8bc6e7b79c6b4ea405d
Emanuel <emanuel@aijunasys.com.ar>
2019-03-24 21:53:18 -0300

    Clase 02: Activities y Fragments

--- app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/MainActivity.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/MainActivity.kt
@@ -1,15 +1,27 @@
 package ar.edu.utn.frba.mobile.a2019c1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainFragment.OnFragmentInteractionListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, MainFragment.newInstance(getString(R.string.hello)))
+                .commit()
+        }
+    }
+
+    override fun onOkTapped() {
+            AlertDialog.Builder(this)
+                .setTitle(R.string.hello)
+                .setPositiveButton(R.string.ok, { _, _ -> })
+                .create().show()
     }
 }
---
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/MainFragment.kt
@@ -0,0 +1,101 @@
+package ar.edu.utn.frba.mobile.a2019c1
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_main.*
+
+
+private const val ARG_TITLE = "title"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [MainFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class MainFragment : Fragment() {
+    private var title: String? = null
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            title = it.getString(ARG_TITLE)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.titleTextView).text = title
+        okButton.setOnClickListener {
+            onButtonPressed()
+        }
+    }
+
+    fun onButtonPressed() {
+        listener?.onOkTapped()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnFragmentInteractionListener {
+        fun onOkTapped()
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param title Title.
+         * @return A new instance of fragment MainFragment.
+         */
+        @JvmStatic
+        fun newInstance(title: String) =
+            MainFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_TITLE, title)
+                }
+            }
+    }
+}
