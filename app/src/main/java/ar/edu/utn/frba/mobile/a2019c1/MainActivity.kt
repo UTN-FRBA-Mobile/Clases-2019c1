a1fba39be412f51f500352c7fad462e4d2721932
vanesaviejo <vanesaviejo@gmail.com>
2019-04-29 01:01:32 -0300

    Código de COnversor de Unidades de Temperatura

--- app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/MainActivity.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/MainActivity.kt
@@ -2,6 +2,8 @@ package ar.edu.utn.frba.mobile.a2019c1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.widget.ImageButton
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,5 +13,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        supportActionBar!!.setTitle("Conversor de Unidades de temperatura")
+
+        val conversor = ConversorUnidades()
+        //val gradosCelsius = temperaturaIngresadaC
+
+
+        val botonC = findViewById(R.id.botonCelsius) as ImageButton
+        botonC.setOnClickListener {
+            // Llamo a celsiustof y celsius to k
+            val valorC = temperaturaIngresadaC.text.toString().toDouble()
+            temperaturaIngresadaF.setText(conversor.celsiusToF(valorC).toString())
+            temperaturaIngresadaK.setText(conversor.celsiusToK(valorC).toString())
+        }
+
+        val botonK = findViewById(R.id.botonKelvin) as ImageButton
+
+        botonK.setOnClickListener {
+            // Llamo a kelvintoc y kelvintof
+            val valorK = temperaturaIngresadaK.text.toString().toDouble()
+            temperaturaIngresadaC.setText(conversor.kelvinToC(valorK).toString())
+            temperaturaIngresadaF.setText(conversor.kelvinToF(valorK).toString())
+        }
+
+        val botonF = findViewById(R.id.botonFahrenheit) as ImageButton
+
+        botonF.setOnClickListener {
+            // Llamo a fahtoc y fahtok
+            val valorF = temperaturaIngresadaF.text.toString().toDouble()
+            temperaturaIngresadaC.setText( conversor.fahToC(valorF).toString())
+            temperaturaIngresadaK.setText(conversor.fahToK(valorF).toString())
+        }
+
     }
+
 }
